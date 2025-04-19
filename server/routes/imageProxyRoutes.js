d5cf62af7bc3212cf7d94b44820fcaaf90c3dcd9
umairumrani <umairdev00@gmail.com>
2025-04-19 11:17:47 +0500

    Remove unnecessary files and directories

--- server/routes/imageProxyRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { proxyImage } = require('../controllers/imageProxyController');
-
-// Proxy image route
-router.get('/proxy', proxyImage);
-
-module.exports = router;
